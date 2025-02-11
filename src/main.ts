07c7fb6ac25ec64293bdd68a281e3e6d72073680
dwhddnjs <per1215@naver.com>
2025-02-11 14:41:08 +0900

    cors 문제 수정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // 모든 도메인에서 접근 가능
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메소드
+    allowedHeaders: 'Content-Type, Accept', // 허용할 헤더
+    credentials: true, // 자격 증명 허용 (쿠키 등)
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
